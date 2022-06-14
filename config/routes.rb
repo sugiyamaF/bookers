cbc46f55d171b97610ffb9f9baf264a11b7a6ad6
EC2 Default User <ec2-user@ip-172-31-29-203.ec2.internal>
2022-06-14 11:32:37 +0000

    bookers

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get root :to => 'homes#top'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  resources :books
+end
